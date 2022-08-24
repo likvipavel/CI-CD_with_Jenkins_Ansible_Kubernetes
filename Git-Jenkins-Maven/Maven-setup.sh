936bad3f6e9f908e6434bec72be3490482bb18ea
Pavel Likvi <likvipavel@bk.ru>
2022-08-24 10:59:05 +0300

    remove some files

--- Git-Jenkins-Maven/Maven-setup.sh
+++ Git-Jenkins-Maven/Maven-setup.sh
@@ -1,15 +1,12 @@
-Jenkins Redhat Packages
+#!/bin/bash
 
-To use this repository, run the following command:
+#Downloading Apache Maven 3.8.6
+  cd /opt
+  wget https://dlcdn.apache.org/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.tar.gz
+  tar -xzvf apache-maven-3.8.6-bin.tar.gz
+  mv apache-maven-3.8.6 maven
 
-
-  sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-  sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-  
-
-If you've previously imported the key from Jenkins, the rpm --import will fail because you already have a key. Please ignore that and move on.
-
-
-  yum install fontconfig java-11-openjdk
-  yum install jenkins
-  
+#Maven Environment Variables:
+  M2_HOME=/opt/maven  #Maven home dir
+  M2=/opt/maven/bin   #path to mvn
+  JAVA_HOME=          #
